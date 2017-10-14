ace059fb4fcf26f9faa784e155faccacb7961978
davidblj <david.blj95@gmail.com>
2017-10-14 10:09:07 -0500

    verificacion de roles para asignacion de cupones

--- server/routes/vendor.js
+++ server/routes/vendor.js
@@ -1,19 +1,29 @@
 let express = require('express');
 let status = require('http-status');
-let mongoose= require('mongoose');
-
+let auth = require('../middleware/auth');
 
 module.exports = function (wagner) {
 
   let api = express.Router();
 
-  api.post('/assignCoupon',wagner.invoke(function(Coupon) {
+  api.post('/assignCoupon', auth.verifyToken, wagner.invoke(function(Coupon) {
       return function(req, res) {
+
             let clientsArray = req.body.clients;
-            let idVendor = req.body.idVendor;
+            let userRole = req.decoded.role;
+            let idVendor = req.decoded._id;
+
+            // todo: remove duplicate code
+            if (userRole !== 'vendor') {
+              return res
+                  .status(status.FORBIDDEN)
+                  .json({error: 'No tienes la autorizacion para este tipo de peticion'});
+            }
 
             Coupon.findOne({id_vendor: idVendor}).exec(function(err, coupon) {
                 if (coupon) {
+
+                    // todo(r2): add a quantity value for users with a coupon from the same vendor
                     Coupon.findOneAndUpdate({id_vendor: idVendor},{ $push: { clients: {$each:clientsArray}}}, function(error) {
                           if (error) {
                               return res
@@ -46,7 +56,5 @@ module.exports = function (wagner) {
           }
   }));
 
-
-
-return api;
-}
+  return api;
+};
