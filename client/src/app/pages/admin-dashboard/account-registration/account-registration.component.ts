e75a17a7af5f93bf56cd422edb8f0196124736ce
davidblj <david.blj95@gmail.com>
2017-11-15 09:33:17 -0500

    Se inicia el servicio para registrar una cuenta

--- client/src/app/pages/admin-dashboard/account-registration/account-registration.component.ts
+++ client/src/app/pages/admin-dashboard/account-registration/account-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-account-registration',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountRegistrationComponent implements OnInit {
 
-  constructor() { }
+  accountForm: FormGroup;
+  accountData;
+
+  constructor(private formBuilder: FormBuilder,
+              ) {
+    this.createForm();
+  }
 
   ngOnInit() {
   }
 
+  createForm() {
+    this.accountForm = this.formBuilder.group({
+      id: '',
+      password: '',
+      balance: ''
+    })
+  }
+
+  onSubmit() {
+    this.accountData = this.accountForm.value;
+    this.accountData['account_number'] = this.createUniqueId();
+
+    console.log(this.accountData);
+    // todo: invoke service
+  }
+
+  createUniqueId(): string {
+    let date = new Date();
+    let components = [
+      date.getMonth(),
+      date.getDate(),
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+    ];
+
+    return components.join("");
+  }
 }
